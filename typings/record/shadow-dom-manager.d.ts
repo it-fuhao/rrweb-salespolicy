591a28ac60dd6fbf27e02e6a3a22dcac2b47b0ad
fuhao <fuhao1px@163.com>
2021-05-27 19:43:27 +0800

    init

---
+++ typings/record/shadow-dom-manager.d.ts
@@ -0,0 +1,27 @@
+import { mutationCallBack, blockClass, maskTextClass, MaskTextFn, Mirror } from '../types';
+import { MaskInputOptions, SlimDOMOptions } from 'snapshot-salespolicy';
+import { IframeManager } from './iframe-manager';
+declare type BypassOptions = {
+    blockClass: blockClass;
+    blockSelector: string | null;
+    maskTextClass: maskTextClass;
+    maskTextSelector: string | null;
+    inlineStylesheet: boolean;
+    maskInputOptions: MaskInputOptions;
+    maskTextFn: MaskTextFn | undefined;
+    recordCanvas: boolean;
+    slimDOMOptions: SlimDOMOptions;
+    iframeManager: IframeManager;
+};
+export declare class ShadowDomManager {
+    private mutationCb;
+    private bypassOptions;
+    private mirror;
+    constructor(options: {
+        mutationCb: mutationCallBack;
+        bypassOptions: BypassOptions;
+        mirror: Mirror;
+    });
+    addShadowRoot(shadowRoot: ShadowRoot, doc: Document): void;
+}
+export {};
